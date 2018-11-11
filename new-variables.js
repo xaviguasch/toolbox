8c28f9abd423c59cf5033fd5d4d4860e63536d68
xaviguasch <xaviguasch@gmail.com>
2018-11-11 15:41:41 +0100

    testing repository

---
+++ new-variables.js
@@ -0,0 +1,19 @@
+var width = 100
+
+console.log(width) // Prints: 100
+
+width = 150
+
+console.log(width) // Prints: 150
+
+
+var width = 200
+
+console.log(width) // Prints: 200
+
+// var variables can be both updated and also reassigned
+
+
+let height = 200
+const key = 'abc123'
+
